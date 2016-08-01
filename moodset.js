6af0a4b4b533052999b61cf9de1f2599d9593486
samcasm <samir7kutty@gmail.com>
2016-08-02 02:16:36 +0530

    Update moodset.js

--- moodset.js
+++ moodset.js
@@ -1,14 +1,10 @@
-(function(){
+button1.addEventListener("click",function(){
 	location.href = "https://accounts.spotify.com/authorize" + 
     			"?client_id=" + "a2675007fa4d48409facbac791af9e2d" +
     			"&response_type=token" + 
-    			"&redirect_uri=" + encodeURIComponent("https://samcasm.github.io/moodsetNow/callback.js") + 
+    			"&redirect_uri=" + encodeURIComponent("https://samcasm.github.io/moodsetNow/moodset.html?callback") + 
     			"";
-})();
-
-function mySpotify(){
-	console.log(response);
-}
+});
 
 
 var button = document.querySelector("button");
