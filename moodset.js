9ea02bf068321bea6b119b513bd2ad730331d058
samcasm <samir7kutty@gmail.com>
2016-08-02 02:21:26 +0530

    Update moodset.js

--- moodset.js
+++ moodset.js
@@ -1,3 +1,5 @@
+var button1 = document.querySelector("#button1");
+
 button1.addEventListener("click",function(){
 	location.href = "https://accounts.spotify.com/authorize" + 
     			"?client_id=" + "a2675007fa4d48409facbac791af9e2d" +
