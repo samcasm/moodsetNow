a61255ae686cf5e02e41fb8567b2dcb339379ad2
samcasm <samir7kutty@gmail.com>
2016-08-02 17:30:58 +0530

    Update moodset.js

--- moodset.js
+++ moodset.js
@@ -8,6 +8,24 @@ button1.addEventListener("click",function(){
     			"";
 });
 
+function mycallback(){
+			var hash = {};
+			location.hash.slice(1).split('&').forEach(function(pair) {
+    			pair = pair.split('=');
+    			hash[pair.shift()] = pair.join('=');
+			});
+			if (hash.error) {
+		    	console.log(hash.error);
+			} else {
+			    token = hash.access_token;
+			    hash.token_type === "Bearer";
+			    var expiry = new Date();
+			    expiry.setSeconds(expiry.getSeconds() + (+hash.expires_in));
+			    console.log(token);
+			    console.log(hash);
+
+			}
+		}
 
 var button = document.querySelector("button");
 
