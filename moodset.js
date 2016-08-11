d5466e4340369669d2de1f3034817a11b17d5f32
samcasm <samir7kutty@gmail.com>
2016-08-12 03:47:14 +0530

    spinner js

--- moodset.js
+++ moodset.js
@@ -50,6 +50,35 @@ weatherButton.addEventListener("click",function(){
 		}
 	}
 	
+	if(!document.querySelector('.body-weather')){
+		//spinner.js
+		var opts = {
+			  lines: 11 // The number of lines to draw
+			, length: 25 // The length of each line
+			, width: 8 // The line thickness
+			, radius: 30 // The radius of the inner circle
+			, scale: 1.25 // Scales overall size of the spinner
+			, corners: 1 // Corner roundness (0..1)
+			, color: '#000' // #rgb or #rrggbb or array of colors
+			, opacity: 0.3 // Opacity of the lines
+			, rotate: 0 // The rotation offset
+			, direction: 1 // 1: clockwise, -1: counterclockwise
+			, speed: 1 // Rounds per second
+			, trail: 60 // Afterglow percentage
+			, fps: 20 // Frames per second when using setTimeout() as a fallback for CSS
+			, zIndex: 2e9 // The z-index (defaults to 2000000000)
+			, className: 'spinner' // The CSS class to assign to the spinner
+			, top: '50%' // Top position relative to parent
+			, left: '50%' // Left position relative to parent
+			, shadow: false // Whether to render a shadow
+			, hwaccel: false // Whether to use hardware acceleration
+			, position: 'absolute' // Element positioning
+		}
+		var target = document.querySelector('.spinner-div-1');
+		var spinner = new Spinner(opts).spin(target);
+	}
+	
+	
 	//take city input
 	var inputCity = document.querySelector(".city").value;
 	///////////////////   GOOGLE MAPS GEOCODER API   ///////////////////////////////
