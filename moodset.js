b6101295256621c55b535fc71ed2a40122a5067b
samcasm <samir7kutty@gmail.com>
2016-08-12 10:14:09 +0530

    function update spinner js

--- moodset.js
+++ moodset.js
@@ -31,39 +31,10 @@ var tracks = []	;
 var weatherButton = document.querySelector(".weather-button");
 
 weatherButton.addEventListener("click",function(){
-	
-	//spinner for show playlists
-	if(document.querySelector('.icon-temperature').innerHTML===""){
-	
-		//spinner.js
-		var opts = {
-			  lines: 11 // The number of lines to draw
-			, length: 25 // The length of each line
-			, width: 8 // The line thickness
-			, radius: 30 // The radius of the inner circle
-			, scale: 0.5 // Scales overall size of the spinner
-			, corners: 1 // Corner roundness (0..1)
-			, color: '#000' // #rgb or #rrggbb or array of colors
-			, opacity: 0.3 // Opacity of the lines
-			, rotate: 0 // The rotation offset
-			, direction: 1 // 1: clockwise, -1: counterclockwise
-			, speed: 1 // Rounds per second
-			, trail: 60 // Afterglow percentage
-			, fps: 20 // Frames per second when using setTimeout() as a fallback for CSS
-			, zIndex: 2e9 // The z-index (defaults to 2000000000)
-			, className: 'spinner' // The CSS class to assign to the spinner
-			, top: '50%' // Top position relative to parent
-			, left: '50%' // Left position relative to parent
-			, shadow: false // Whether to render a shadow
-			, hwaccel: false // Whether to use hardware acceleration
-			, position: 'absolute' // Element positioning
-		}
-	
-		var target = document.querySelector('.spinner-div-1');
-		
-		globals['spinner'] = new Spinner(opts).spin(target);
-	
-	}
+	//invoking the startSpinner function
+	var targetEl = document.querySelector('.spinner-div-1');
+	var checkEl = document.querySelector('.icon-temperature');
+	startSpinner(targetEl,checkEl);
 	
 	
 	//clear preiously executed queries
@@ -465,3 +436,36 @@ function secondsToTime(secs){
     return obj;
 }
 
+function startSpinner(checkEl,targetEl){
+	if(checkEl.innerHTML===" "){
+	
+		//spinner.js
+		var opts = {
+			  lines: 11 // The number of lines to draw
+			, length: 25 // The length of each line
+			, width: 8 // The line thickness
+			, radius: 30 // The radius of the inner circle
+			, scale: 0.5 // Scales overall size of the spinner
+			, corners: 1 // Corner roundness (0..1)
+			, color: '#000' // #rgb or #rrggbb or array of colors
+			, opacity: 0.3 // Opacity of the lines
+			, rotate: 0 // The rotation offset
+			, direction: 1 // 1: clockwise, -1: counterclockwise
+			, speed: 1 // Rounds per second
+			, trail: 60 // Afterglow percentage
+			, fps: 20 // Frames per second when using setTimeout() as a fallback for CSS
+			, zIndex: 2e9 // The z-index (defaults to 2000000000)
+			, className: 'spinner' // The CSS class to assign to the spinner
+			, top: '50%' // Top position relative to parent
+			, left: '50%' // Left position relative to parent
+			, shadow: false // Whether to render a shadow
+			, hwaccel: false // Whether to use hardware acceleration
+			, position: 'absolute' // Element positioning
+		}
+	
+	
+		
+		globals['spinner'] = new Spinner(opts).spin(targetEl);
+	
+	}
+}
