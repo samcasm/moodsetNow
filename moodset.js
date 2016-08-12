20619cb7977d1b06592c5e8efabf082480a4a243
samcasm <samir7kutty@gmail.com>
2016-08-12 11:17:11 +0530

    fgfgfg

--- moodset.js
+++ moodset.js
@@ -267,41 +267,11 @@ finalPlaylistButton.addEventListener("click",makeFinalPlaylist);
 
 //display final mashed up playlist 
 function makeFinalPlaylist(){
+	//invoke the startSpinner function for the final mashed playlist
+	var checkEl = document.querySelector('.final-playlist');
+	var targetEl = document.querySelector('.spinner-div-3');
+	startSpinner(checkEl,targetEl);
 	
-	//spinner for show playlists
-	if(document.querySelector('.final-playlist').innerHTML===" "){
-	
-		//spinner.js
-		var opts = {
-			  lines: 11 // The number of lines to draw
-			, length: 25 // The length of each line
-			, width: 8 // The line thickness
-			, radius: 30 // The radius of the inner circle
-			, scale: 0.5 // Scales overall size of the spinner
-			, corners: 1 // Corner roundness (0..1)
-			, color: '#000' // #rgb or #rrggbb or array of colors
-			, opacity: 0.3 // Opacity of the lines
-			, rotate: 0 // The rotation offset
-			, direction: 1 // 1: clockwise, -1: counterclockwise
-			, speed: 1 // Rounds per second
-			, trail: 60 // Afterglow percentage
-			, fps: 20 // Frames per second when using setTimeout() as a fallback for CSS
-			, zIndex: 2e9 // The z-index (defaults to 2000000000)
-			, className: 'spinner' // The CSS class to assign to the spinner
-			, top: '50%' // Top position relative to parent
-			, left: '50%' // Left position relative to parent
-			, shadow: false // Whether to render a shadow
-			, hwaccel: false // Whether to use hardware acceleration
-			, position: 'absolute' // Element positioning
-		}
-	
-		var target = document.querySelector('.spinner-div-3');
-		
-		globals['spinner'] = new Spinner(opts).spin(target);
-	
-		
-	
-	}
 	
 	//filter tracks for promos and ads that are 60secs or less duration
 	tracks = tracks.filter(function(track){
@@ -354,12 +324,9 @@ function makeFinalPlaylist(){
 		var finalPlaylist = document.querySelector(".final-playlist");
 		finalPlaylist.appendChild(tr);
 	}
-	//stop spinner for final playlist
-	if(!(document.querySelector('.final-playlist').innerHTML===" ")){
 	
-		globals['spinner'].stop();
-		
-	}
+	//invoking the stopSpinner for the final playlist
+	stopSpinner(checkEl);
 	
 	document.querySelector(".final-playlist-table").style.visibility = "visible";
 
